c0ab9b28fe0cdc758bd7c11e60a6a19cc26f0b06
James <jthomas302@gmail.com>
2021-01-04 14:51:48 -0500

    add conditionals

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ var app = new Vue ({
         product: 'Socks',
         description: 'Green Socks',
         image: './assets/vmSocks-green-onWhite.jpg',
-        image_link: ('https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg')
-    
+        image_link: ('https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'),
+        // inStock: true,
+        inventory: 100,
+        onSale: false
+
+
+
+
+
     
     
     }
