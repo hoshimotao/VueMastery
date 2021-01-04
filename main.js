e220450d2ef357a158a0bf442749dcc0642ac96d
James <jthomas302@gmail.com>
2021-01-04 15:09:08 -0500

    add loops arrays details objects

--- main.js
+++ main.js
@@ -7,7 +7,30 @@ var app = new Vue ({
         image_link: ('https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'),
         // inStock: true,
         inventory: 100,
-        onSale: false
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-Neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+        sizes: [
+               { sizesId: 2235,
+                size: "small"
+               },
+               { sizesId: 2236,
+                size: "medium"
+               },
+               { sizesId: 2237,
+                size: "large"
+               },
+
+        ]
 
 
 
